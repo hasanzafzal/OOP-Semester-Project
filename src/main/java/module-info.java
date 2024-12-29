a6b0dd38bc2982030105ad19b4b64847869f6240
Hasan Zahid <146334715+hasanzafzal@users.noreply.github.com>
2024-12-29 22:35:25 +0500

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.parkingmanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.parkingmanagement to javafx.fxml;
-    exports com.example.parkingmanagement;
-}
