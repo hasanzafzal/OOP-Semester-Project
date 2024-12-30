7ca7fd2c5ae04230d1a614a74b5636637bce26bd
Hasan Zahid <146334715+hasanzafzal@users.noreply.github.com>
2024-12-31 02:49:27 +0500

    Deleted

--- OOP Front Project/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.oopfrontproject {
-    requires java.sql;
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.example.oopfrontproject to javafx.fxml;
-    exports com.example.oopfrontproject;
-}
